ff870f3e4ff81f1acc08adb19c020252be51b84d
Ettore Marques <emcimino1@hotmail.com>
2021-07-05 12:51:02 -0300

    replay perfeição

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 window.onload = function () {
     document.getElementById('comecar').addEventListener('click', function () {
-        debugger
         document.getElementById('comecar').classList.toggle('hidden')
         document.querySelector('.video').classList.toggle('hidden')
 
         document.querySelector('video').play()
         document.querySelector('audio').play()
+        
+        setInterval(function () {
+            document.querySelector('video').currentTime = 0
+            document.querySelector('audio').currentTime = 0
+            document.querySelector('video').play()
+            document.querySelector('audio').play()
+        }, 71100)
     })
 }
